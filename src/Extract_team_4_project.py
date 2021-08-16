2f3551ff864babbc4fd40231fd3e33d168a09926
Bayo19 <bayo.300@gmail.com>
2021-08-16 15:34:51 +0100

    removed sensitive data, still need to remove the extra commas

--- src/Extract_team_4_project.py
+++ src/Extract_team_4_project.py
@@ -1,10 +1,11 @@
-from csv import DictReader
+from csv import reader
+
 
 customers_list = []
 with open('2021-02-23-isle-of-wight.csv') as cafe_file:
-    cafe_list = DictReader(cafe_file)
+    cafe_list = reader(cafe_file)
     for cafe_items in cafe_list:
         customers_list.append(cafe_items)
         
-print(customers_list)
+
     
---
+++ src/Transform_team_4_project.py
@@ -0,0 +1,10 @@
+import Extract_team_4_project as extract
+import pprint
+removed_sensitive_data = []
+
+for row in extract.customers_list:
+    row.pop(2)
+    row.pop(-1)
+    removed_sensitive_data.append(row)
+    
+pprint.pprint(removed_sensitive_data[:4])
