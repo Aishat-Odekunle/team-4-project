aa3a304ee4c633596499172e6bd7d4a3ec604134
Bayo19 <bayo.300@gmail.com>
2021-08-17 15:50:48 +0100

    transform function mostly done, need to revise it again

--- src/Transform_team_4_project.py
+++ src/Transform_team_4_project.py
@@ -1,28 +1,57 @@
 import Extract_team_4_project as extract
+import pprint
 
+clean_data = []
 
-removed_sensitive_data = []
-
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+    
 def transform_data(list_to_read_from, list_to_append_to):
     for row in list_to_read_from:
         row.pop(2)
         row.pop(-1)
-        list_to_append_to.append(row)
-
-    # removing extra commas in place, in the order column    
-    for row in list_to_append_to:
         order = row[2].split(',')
+        
+        products = []
+        prices = []
         
         for i in order:
             if i == '':
                 order.remove(i)
-        row[2] = ', '.join(order)
+        row[2] = ','.join(order)
+        
+        for i in row[2].split(','):
+            if is_number(i):
+                prices.append(i)
+        
+        order_list = row[2].split(',')
+        for i in range(len(order_list)):
+            if is_number(order_list[i]):
+                order_list[i] = ':'
+        order_list = ''.join(order_list)
+        order_list = order_list.split(':')
+        
+        for i in range(len(order_list)):
+            if order_list[i] != '':
+                products.append(order_list[i])
+                
+        transformed = {}
+        transformed["timestamp"] = row[0]
+        transformed["branch"] = row[1]
+        transformed["basket"] = [{"name": f, "price": b} for f, b in zip(products, prices)]
+        transformed["payment type"] = row[3]
+        transformed["total"] = row[4]
+            
+        list_to_append_to.append(transformed)
+    
+    
+transform_data(extract.customers_list, clean_data)
 
-transform_data(extract.customers_list, removed_sensitive_data)
 
-
-
-
-
+pprint.pprint(clean_data[:5])
 
 
