f5ba86015027ed817c8777851b963618a75beb68
Bayo19 <bayo.300@gmail.com>
2021-08-28 12:14:50 +0100

    handler file update, events commented out

--- src/team-4-etl/handler.py
+++ src/team-4-etl/handler.py
@@ -1,10 +1,21 @@
 import json
+import boto3
+
+# def hello(event, context):
+#     body = {
+#         "message": "Go Serverless v2.0! Your function executed successfully!",
+#         "input": event,
+#     }
+
+#     return {"statusCode": 200, "body": json.dumps(body)}
+
+def start(event, context):
+    s3 = boto3.resource('s3', region_name="eu-west-1")
+    print(event['Records'])
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v2.0! Your function executed successfully!",
-        "input": event,
-    }
-
-    return {"statusCode": 200, "body": json.dumps(body)}
+# 1. process the json
+# need boto to call out to aws and get s3 file
+# check if last four chars is '.csv'
+# uri encoding
+# figure out how to get files from s3
