f99886887c42c3e2a1aab58f5befedb963c729c9
Bayo19 <bayo.300@gmail.com>
2021-08-31 16:37:09 +0100

    handler working, need to discuss

--- src/team-4-etl/handler.py
+++ src/team-4-etl/handler.py
@@ -12,24 +12,12 @@ import csv
 #     }
 
 #     return {"statusCode": 200, "body": json.dumps(body)}
-
-# 1. process the json
-# need boto to call out to aws and get s3 file
-# check if last four chars is '.csv'
-# uri encoding
-# figure out how to get files from s3
-
-
-# def start(event, context):
-#     s3_client = boto3.client('s3')
-#     for record in event['Records']:
-#         key = unquote_plus(record['s3']['object']['key'])
-#         s3_client.download_file('gen-x1-team-4-data-in', key, 'dl.csv')
        
 def start(event, context):
     key = event['Records'][0]['s3']['object']['key']
     bucket = event['Records'][0]['s3']['bucket']['name']
     s3_client = boto3.client('s3')
-    s3_obj = s3_client.get_object(Bucket=bucket, Key=key)
-    data = s3_obj['Body'].read().decode('utf-8')
+    s3_object = s3_client.get_object(Bucket=bucket, Key=key)
+    data = s3_object['Body'].read().decode('utf-8')
     return data
+
